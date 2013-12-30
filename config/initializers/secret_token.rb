266691ab3ae2dcddbb7447c51a45c22950092d33
yesdnil5 <yesdnil5@gmail.com>
2013-12-29 21:38:46 -0500

    setting things up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Texta::Application.config.secret_key_base = 'c5bba8eb89908ab540b9b024986f15664a17f2077ba05428cec71a0d4693df19b419012feb0032c08a86ce072acdff14cd3276d5efba6d46e8ed9e72ee65fdbd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Texta::Application.config.session_store :cookie_store, key: '_texta_session'
